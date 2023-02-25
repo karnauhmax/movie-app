8172db2d21b2eead496814f508c03a94fb3861f4
karnauhmax <krnhmx@gmail.com>
2023-02-25 19:58:18 +0300

    updated render data function

--- src/js/components/renderDataList.js
+++ src/js/components/renderDataList.js
@@ -1,18 +1,19 @@
 import genres from './genres/genresList';
 import renderGenres from './genres/renderGenres';
 import createDynamicLinks from './links/createDynamicLinks';
+import determineType from './links/determineType';
 import linksList from './links/linksList';
 import renderParamsLinks from './links/renderParamsLinks';
-
+import clearHTML from './clear/clearHTML';
 const loader = document.querySelectorAll('.loader');
-const renderDataList = (dataObj, filmsInner) => {
+const renderDataList = (dataObj, filmsInner, classList = '') => {
   console.log(dataObj);
   loader.forEach(loader => {
     loader.classList.remove('active');
   });
   const genresList = document.querySelectorAll('.item__genres');
   const filmsList = dataObj.results;
-  filmsInner.innerHTML = '';
+  clearHTML(filmsList);
   filmsList.forEach(film => {
     //destructuring
     const {
@@ -27,35 +28,23 @@ const renderDataList = (dataObj, filmsInner) => {
       id
     } = film;
 
-    //genres detecting
-    // const genresResult = [];
-    // if (genre_ids && genre_ids.length >= 1) {
-    //   genres.forEach(genreObj => {
-    //     genre_ids.forEach(item => {
-    //       if (item == genreObj.id) {
-    //         genresResult.push(genreObj.name);
-    //       }
-    //     });
-    //   });
-    // } else if (known_for && known_for.length >= 1) {
-    //   known_for.forEach(film => {
-    //     genresResult.push(film.title || film.name);
-    //   });
-    // }
-
     const genresResult = renderGenres(genre_ids, known_for);
 
     //rendering films/actors/movies by data
     filmsInner.insertAdjacentHTML(
       'beforeend',
       `
-    <article class="item">
-      <a href="single.html?id=${id}" class="item__link">
-        <div href="#" class="item__img-wrapper">
+    <article class="item ${
+      typeof classList === 'object' ? classList.join(' ') : classList
+    }">
+      <a href="single.html?media=${determineType(
+        film
+      )}&id=${id}" class="item__link">
+        <div class="item__img-wrapper">
             <div class="item__img ibg">
                 ${
                   poster_path || profile_path
-                    ? `<img " src="https://image.tmdb.org/t/p/w200/${
+                    ? `<img src="https://image.tmdb.org/t/p/w200/${
                         poster_path || profile_path
                       }" class="skeleton-image" width="250" height="250"alt="">`
                     : '<p>Oops :( Looks like image not found</p>'
